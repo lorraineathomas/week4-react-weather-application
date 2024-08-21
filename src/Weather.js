ead46bb72e074b4ba1db9dbf183f8ea71f138820
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-08-21 15:00:32 -0400

    Added script.js content to Weather.js file above return

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,61 @@ import React, {useState} from "react";
 import "./Weather.css";
 
 export default function Weather() {
+
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperatureValue = Math.round(convertTemperature(response.data.temperature.current));
+    temperatureElement.innerHTML = temperatureValue;
+    
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.main.condition.description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+
+    let windElement = document.querySelector("#wind-speed");
+    let speed = Math.round(response.data.wind.speed * 1.609344);
+    windElement.innerHTML = speed;
+
+    let dayTime = new Date(response.data.time * 1000);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    timeElement = document.querySelector("#time");
+    let minutes = dayTime.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${dayTime.getMinutes()}`;
+    }
+    timeElement.innerHTML = `${days[dayTime.getDay()]} ${dayTime.getHours()}:${minutes}`;
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    }
+
+    function searchCity(city){
+    let apiKey = "bf54175800a55e59e6c4d6461deeef12";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+    }
+
+    function convertTemperature(celcius){
+    let temp = 9/5 * celcius + 32;
+    return temp;
+    }
+
+    function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
+    }
+
+    let searchFormElement = document.querySelector("#search-form");
+    searchFormElement.addEventListener("submit", handleSearchSubmit);
+
         return (
             <div className="Weather">
+                <div class="weather-app">
                  <header>
                 <form class="search-form" id="search-form">
                     <input class="search-form-input" id="search-form-input" type="search" placeholder="Enter a city..." required />
@@ -32,10 +85,11 @@ export default function Weather() {
 
                 <footer>
                 Created by 
-                <a href="https://github.com/lorraineathomas"> Lorraine Thomas</a> and is 
-                <a href="https://github.com/lorraineathomas/weather-app-project">hosted on Github</a>
-                and <a href="https://another-weather-search.netlify.app/">hosted on Netlify</a>
+                <a href="https://github.com/lorraineathomas"> Lorraine Thomas</a>{" "} and is{" "}
+                <a href="https://github.com/lorraineathomas/weather-app-project">{" "}hosted on Github{" "}</a>
+                and{" "}<a href="https://another-weather-search.netlify.app/">{" "}hosted on Netlify</a>
                 </footer>
+                </div>
             </div>
         )
 }
